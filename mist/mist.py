ac1ec88f712bb57df91b078e34e14146a96a6a2e
Dillon Barker <dor.barker@gmail.com>
2018-03-23 18:39:06 -0600

    result parsing

--- mist/mist.py
+++ mist/mist.py
@@ -20,8 +20,6 @@ def arguments():
                         required=True,
                         help='Test info file')
 
-    parser.add_argument
-
     parser.add_argument('-a', '--alleles',
                         type=Path,
                         required=True,
@@ -34,7 +32,6 @@ def arguments():
 
     args = parser.parse_args()
 
-    
     if not args.test.exists():
         msg = 'Test file {} does not exist'.format(args.test)
         logging.error(msg)
@@ -55,25 +52,54 @@ def main():
 
 def blast(query_path: Path, genome_path: Path) -> pd.DataFrame:
 
-    out_format = '"6 qseqid sseqid pident length qstart qend sstart send qlen \
-                  bitscore gaps sseq qseq mismatch"'
+    out_format = '6 qseqid sseqid pident length qstart qend sstart send qlen \
+                  slen bitscore gaps sseq qseq mismatch'.split()
     
-    blast_cmd = ('blastn',
+    blast_cmd = ['blastn',
                  '-query', query_path,
                  '-subject', genome_path,
-                 '-outfmt', out_format)
+                 '-outfmt'] + out_format
 
     result = subprocess.run(blast_cmd, check=True, stdout=subprocess.PIPE)
 
     blast_output = pd.read_csv(io.BytesIO(result.stdout), sep='\t', header=0)
 
-    blast_output['reverse_complement'] = blast_output.sstart > blast_output.send
-    
-    temp = blast_output[blast_output.reverse_complement]
-
-    blast_output.loc[blast_output.reverse_complement, 'send'] = temp.sstart
-
-    blast_output.loc[blast_output.reverse_complement, 'sstart'] = temp.send
-
     return blast_output
 
+
+def parse_blast_results(blast_output: pd.DataFrame) -> pd.DataFrame:
+
+
+    def is_contig_truncation(row):
+
+        if not row['qlen'] > row['length']:
+            return False
+
+        if row['reverse_complement']:
+
+           return row['sstart'] == row['slen'] or row['send'] == 1
+
+        else:
+
+            return row['slen'] == row['send'] or row['sstart'] == 1
+
+    def is_correct(row):
+
+        correct = (row['mismatch'] == 0) & \
+                  (row['pident'] == 100) & \
+                  (row['qlen'] == row['slen']) & \
+                  (row['gaps'] == 0)
+
+        return correct
+
+    blast_output['reverse_complement'] = blast_output.sstart > blast_output.send
+
+    correct = blast_output.apply(is_correct, axis=1)
+
+    blast_output['correct'] = correct
+
+    is_truncation = blast_output.apply(is_contig_truncation, axis=1)
+
+    blast_output['is_contig_truncation'] = is_truncation
+
+    return blast_output
