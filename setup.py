d46c3d7e87b57e6b1a0b18067faac27ce4b61536
Dillon Barker <dor.barker@gmail.com>
2018-09-17 12:24:49 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name='fsac',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=0.22.0'
+    ],
+
+    author='Dillon Barker',
+    author_email='dillon.barker@canada.ca',
+
+    entry_points={
+        'console_scripts': [
+            'fsac = fsac:main'
+        ]
+    }
+)
